c59f6de2c5db21984526b309ef7dc1a8958b5ab2
Minal Patil <patilminal322@gmail.com>
2025-04-04 20:25:56 +0530

    Added support for json input,and parametrized fixutres

--- tests/test_shopping_online.py
+++ tests/test_shopping_online.py
@@ -1,28 +1,27 @@
 from pages.shopping import ShopProducts
 from pages.checkout import CheckoutPayment
+import pytest
+import json
+from pathlib import Path
+file_path = Path(__file__).parent.parent / "config" / "input_data.json"
+print(file_path)
 
+with open(file_path) as curr_file:
+    test_data = json.load(curr_file)
+    data_list = test_data["datas"]
+    print(data_list)
 
-def test_online_shopping(browserInstance):
+@pytest.mark.parametrize("new_data_list",data_list)
+def test_online_shopping(browserInstance,new_data_list):
     driver = browserInstance
-    product_name = "OnePlus 9 Pro"
-    quantity = 2
-    email = "patilminal322@gmail.com"
-    shipping_method ="Fast shipping"
-    card_details = {"card_number":"4242424242424242", "expiry_date": "03/26" , "cvc":"235"}
-    card_holder_name = "Minal Patil"
-    country = "India"
-    address= "bs"
-    city = "Pune"
-    pincode= "411048"
-    state = "Maharashtra"
 
     #shop product and add to cart
     shop_obj = ShopProducts(driver)
-    shop_obj.select_product(product_name)
-    shop_obj.add_product_to_cart(quantity)
+    shop_obj.select_product(new_data_list["product_name"])
+    shop_obj.add_product_to_cart(int(new_data_list["quantity"]))
 
     #Checkout and confirmation
     checkout = CheckoutPayment(driver)
     checkout.go_to_cart()
-    checkout.checkout(email,shipping_method,card_details,card_holder_name,country,address,city,pincode,state)
+    checkout.checkout(new_data_list)
     checkout.submit_payment()
