436001e4ceede996faad1fed0f5e6552abd78fa4
Minal Patil <patilminal322@gmail.com>
2025-04-04 20:26:44 +0530

    Added code for optimization and user input values for multiple tests

--- pages/checkout.py
+++ pages/checkout.py
@@ -11,63 +11,71 @@ class CheckoutPayment:
         self.go_cart_button = (By.CSS_SELECTOR, ".cart-icon")
         self.payment_button = (By.XPATH, "//div/button[text()='Pay with Stripe']")
         self.email_input= (By.ID,"email")
+        self.fast_shipping_mode = (By.XPATH,"//div[.='Fast Shipping in India']")
+        self.free_shipping_mode = (By.XPATH, "//label[contains(@aria-label,'Free Shipping')]")
+        self.card_number = (By.ID,"cardNumber")
+        self.card_expiry = (By.ID, "cardExpiry")
+        self.card_cvc = (By.ID, "cardCvc")
+        self.card_holder = (By.ID, "billingName")
+        self.country = (By.CSS_SELECTOR,".Select-source")
+        self.city =(By.ID, "billingLocality")
+        self.pincode = (By.ID, "billingPostalCode")
+        self.address = (By.ID,"billingAddressLine1")
+        self.submit_payment_button = (By.CSS_SELECTOR,".SubmitButton")
 
     def go_to_cart(self):
-        # Go to Cart
-        self.driver.find_element(By.CSS_SELECTOR, ".cart-icon").click()
+        # Go to Cart button
+        self.driver.find_element(*self.go_cart_button).click()
+        # Pay with stripe button
+        self.driver.find_element(*self.payment_button).click()
 
-        # Pay with stripe
-        self.driver.find_element(By.XPATH, "//div/button[text()='Pay with Stripe']").click()
-        wait = WebDriverWait(self.driver, 10)
-        wait.until(expected_conditions.visibility_of_element_located((By.XPATH, "//div/span[.='Pay E-commerce']")))
-        message = self.driver.find_element(By.XPATH, ".//div/span[.='Pay E-commerce']").text
-        print(message)
+    def checkout(self,new_data_list):
+        shipping_method = new_data_list["shipping_method"]
+        card_details = new_data_list["card_details"]
 
-    def checkout(self,email,shipping_method,card_details,card_holder_name,country,address,city,pincode,state):
         #Enter User details
-        self.driver.find_element(*self.email_input).send_keys(email)
+        self.driver.find_element(*self.email_input).send_keys(new_data_list["email"])
         ship_amount =""
 
         #Select mode of shipping
         if "Fast" in shipping_method or "fast" in shipping_method:
             print("inside fast")
-            self.driver.find_element(By.XPATH,"//div[.='Fast Shipping in India']").click()
-            # //div[@class='ShippingSelector-price']/span[.='₹60.00']
+            self.driver.find_element(*self.fast_shipping_mode).click()
             ship_amount = self.driver.find_element(By.XPATH,"//div[@class='ShippingSelector-price']/span[.='₹60.00']").text
             print(shipping_method)
             print(ship_amount)
         elif "Free" in shipping_method or "free" in shipping_method:
             #//label[contains(@aria-label,'Free Shipping')]
-            self.driver.find_element(By.XPATH, "//label[contains(@aria-label,'Free Shipping')]").click()
+            self.driver.find_element(*self.free_shipping_mode).click()
+            print(shipping_method)
 
         #Enter card Details
-        self.driver.find_element(By.ID,"cardNumber").send_keys(card_details["card_number"])
-        self.driver.find_element(By.ID, "cardExpiry").send_keys(card_details["expiry_date"])
-        self.driver.find_element(By.ID, "cardCvc").send_keys(card_details["cvc"])
-        self.driver.find_element(By.ID, "billingName").send_keys(card_holder_name)
+        self.driver.find_element(*self.card_number).send_keys(card_details["card_number"])
+        self.driver.find_element(*self.card_expiry).send_keys(card_details["expiry_date"])
+        self.driver.find_element(*self.card_cvc).send_keys(card_details["cvc"])
+        self.driver.find_element(*self.card_holder).send_keys(new_data_list["card_holder_name"])
 
         #Enter Country
-        select_obj = Select(self.driver.find_element(By.CSS_SELECTOR,".Select-source"))
-        select_obj.select_by_visible_text(country)
+        select_obj = Select(self.driver.find_element(*self.country))
+        select_obj.select_by_visible_text(new_data_list["country"])
 
         #Enter Address
-        address1= self.driver.find_element(By.ID,"billingAddressLine1")
+        address1= self.driver.find_element(*self.address)
         self.driver.execute_script("arguments[0].setAttribute('autocomplete', 'off')",address1 )
-        address1.send_keys(address)
+        address1.send_keys(new_data_list["address"])
         #self.driver.find_element(By.TAG_NAME, "body").click()
 
         # self.driver.find_element(By.ID, "billingAddressLine2").send_keys(address)
         # self.driver.find_element(By.TAG_NAME, "body").click()
-        self.driver.find_element(By.ID, "billingLocality").send_keys(city)
-        self.driver.find_element(By.ID, "billingPostalCode").send_keys(pincode)
+        self.driver.find_element(*self.city).send_keys(new_data_list["city"])
+        self.driver.find_element(*self.pincode).send_keys(new_data_list["pincode"])
 
         self.driver.execute_script("window.scrollBy(0,document.body.scrollHeight);")
-        select_obj = Select(self.driver.find_element(By.ID, "billingAdministrativeArea"))
-        select_obj.select_by_visible_text(state)
+        state_elements = self.driver.find_elements(By.ID, "billingAdministrativeArea")
+        if state_elements and state_elements[0].is_displayed():
+            select_obj = Select(state_elements[0])
+            select_obj.select_by_visible_text(new_data_list["state"])
         time.sleep(5)
-
-        print("Entered all details")
-
 
     def submit_payment(self):
         #go to end of page
@@ -75,9 +83,9 @@ class CheckoutPayment:
         self.driver.execute_script("window.scrollBy(0,document.body.scrollHeight);")
         time.sleep(2)
         print("Submit payment")
-        #self.driver.find_element(By.TAG_NAME, "body").click()
+        self.driver.find_element(By.TAG_NAME, "body").click()
         #click on pay button
-        self.driver.find_element(By.CSS_SELECTOR,".SubmitButton").click()
+        self.driver.find_element(*self.submit_payment_button).click()
         wait = WebDriverWait(self.driver,10)
         wait.until(expected_conditions.visibility_of_element_located((By.TAG_NAME, "h2")))
         message = self.driver.find_element(By.TAG_NAME, "h2").text
